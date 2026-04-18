ec73717b54afa3d14b543c83ea98681d544008ea
harishkumarcsfdataai-debug <harishkumarc.sfdataai@gmail.com>
2026-04-18 10:38:36 +0530

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 import snowflake.connector
 from langchain_groq import ChatGroq
-import os
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = FastAPI()
 
